2e57581294706c060e48e7e2b13166703c4fce7e
Krystian Piękoś <krystian.piekos@infotraining.pl>
2026-09-14 11:27:27 +0200

    Basic types

--- first_app/src/main.rs
+++ first_app/src/main.rs
@@ -1,5 +1,18 @@
-fn main() {
-    let msg = "Hello, world!";
+static MESSAGE: &str = "Hello Rust!";
+static mut COUNTER: u32 = 0;
 
-    println!("First App: {}", msg);
+fn click() {
+    unsafe {
+        COUNTER += 1;
+    }
+}
+
+fn main() {
+    println!("{}", MESSAGE);
+    click();
+    click();
+    unsafe {
+        let current_state_of_counter = COUNTER;
+        println!("COUNTER: {}", current_state_of_counter);
+    }
 }
